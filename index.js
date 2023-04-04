a20c84f369262390fbe0ed616252cc7b5b7f1125
AndrMshk <andrmshk1@gmail.com>
2023-04-04 13:52:38 +0300

    add env

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ import express from 'express'
 import mongoose from 'mongoose'
 import router from "./router.js";
 import fileUpload from 'express-fileupload'
+import dotenv from "dotenv";
 
-const PORT = 6000
-
-const DB_URL = 'mongodb+srv://andr:andrey031444@cluster.4l8v2r8.mongodb.net/?retryWrites=true&w=majority'
 
 const app = express()
+dotenv.config()
 
 app.use(express.json())
 app.use(express.static('static'))
@@ -16,8 +15,8 @@ app.use('/', router)
 
 const startApp = async () => {
   try {
-    await mongoose.connect(DB_URL)
-    app.listen(PORT, () => {
+    await mongoose.connect(process.env.DB_URL)
+    app.listen(process.env.PORT || 5000, () => {
       console.log('server ready')
     })
   } catch (err) {
